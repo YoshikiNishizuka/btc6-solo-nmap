7cb48dc0f91cebb420bf05803f5f0fe720c6241c
NISHIZUKA Yoshiki <116330146+YoshikiNishizuka@users.noreply.github.com>
2024-05-27 10:19:46 +0900

    直したい部分ありつつMVP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,29 @@
 import { useState, useEffect } from "react";
 import "leaflet/dist/leaflet.css";
-import Leaflet from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import './App.css'
+import "./App.css";
+import CurrentPostion from "./components/CurrentPosition";
+import Nall from "./components/Nall";
+import Nearest from "./components/Nearest";
+import Submit from "./components/Submit";
+
 // 現在地取得
 const getCurrentPosition = () =>
   new Promise((resolve, reject) =>
     navigator.geolocation.getCurrentPosition(resolve, reject)
   );
 
-// 現在地アイコン=>未実装
-const currentIcon = Leaflet.icon({
-  // iconUrl: './mapIcon/icon1.png',
-  iconSize: [40, 40],
-});
-// 検索値アイコン=>未実装
-const placeIcon = Leaflet.icon({
-  // iconUrl: "./mapIcon/icon2.png",
-  iconSize: [40, 40],
-  className: "placeIcon",
-});
+// // 現在地アイコン=>未実装
+// const currentIcon = Leaflet.icon({
+//   // iconUrl: './mapIcon/icon1.png',
+//   iconSize: [40, 40],
+// });
+// // 検索値アイコン=>未実装
+// const placeIcon = Leaflet.icon({
+//   // iconUrl: "./mapIcon/icon2.png",
+//   iconSize: [40, 40],
+//   className: "placeIcon",
+// });
 
 const App = () => {
   const [mapKey, setMapKey] = useState(0); //Map再描画用
@@ -56,25 +60,110 @@ const App = () => {
       .then((data) => setPlaceData(data));
   };
 
+  //経度緯度から２点間の距離を返す
+  const R = Math.PI / 180;
+  function distance(lat1, lng1, lat2, lng2) {
+    lat1 *= R;
+    lng1 *= R;
+    lat2 *= R;
+    lng2 *= R;
+    return (
+      6371 *
+      Math.acos(
+        Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) +
+          Math.sin(lat1) * Math.sin(lat2)
+      )
+    );
+  }
+
   const getNearestList = async () => {
     setMapzoom("16");
     const location = await getCurrentPosition();
+    // console.log(location)
     setCurrentPosition({
       lat: location.coords.latitude,
       lng: location.coords.longitude,
     });
+    // console.log(currentPosition)
     setMapKey(new Date().getTime());
+    placeData.forEach(
+      (obj) =>
+        (obj.distance =
+          distance(
+            currentPosition.lat,
+            currentPosition.lng,
+            obj.lat,
+            obj.lng
+          ).toFixed(3) + "km")
+    );
+    placeData.sort((a, b) => {
+      if (a.distance < b.distance) return -1;
+      if (a.distance > b.distance) return 1;
+      return 0;
+    });
   };
+
+  // function deleteMark(ele) {
+  //   console.log(ele);
+  //   const id=ele.id
+  //   console.log(id)
+  //   // fetch(`/api/list/${id}`,{
+  //   //   method:'DELETE'
+  //   // });
+  // }
+
+  const [addAddress, setAddAddress] = useState("");
+  const [addName, setAddName] = useState("");
+  const [addLat, setAddLat] = useState("");
+  const [addLng, setAddLng] = useState("");
+
+  function clickSubmit() {
+    fetch("/api/list", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: addName,
+        address: addAddress,
+        latitude: addLat,
+        longitude: addLng,
+      }),
+    });
+    getAllList();
+  }
 
   return (
     <>
-      <h1>N-map </h1><h2>~長久手市 公共トイレ検索 非公式ツール~</h2>
+      <h1>N-map </h1>
+      <h2>~長久手市 公共トイレ検索 非公式ツール~</h2>
       <div>
-        <button onClick={() => moveCurrentPosition()}>現在地へ移動</button>
-        <button onClick={() => getAllList()}>長久手全表示</button>
-        <button onClick={() => getNearestList()}>近い順検索</button>
+        <CurrentPostion onClick={() => moveCurrentPosition()}></CurrentPostion>
+        <Nall onClick={() => getAllList()}></Nall>
+        <Nearest onClick={() => getNearestList()}></Nearest>
       </div>
       {/* 地図表示 */}
+      <input
+        type="textbox"
+        placeholder="住所"
+        onChange={(event) => setAddAddress(event.target.value)}
+      />
+      <input
+        type="textbox"
+        placeholder="施設or場所名"
+        onChange={(event) => setAddName(event.target.value)}
+      />
+      <input
+        type="textbox"
+        placeholder="緯度 latitude"
+        onChange={(event) => setAddLat(event.target.value)}
+      />
+      <input
+        type="textbox"
+        placeholder="経度 longitude"
+        onChange={(event) => setAddLng(event.target.value)}
+      />
+      <Submit className="add_point_submit" onClick={clickSubmit}></Submit>
       <MapContainer
         id="map"
         key={mapKey}
@@ -98,15 +187,27 @@ const App = () => {
         {placeData.length > 0
           ? placeData.map((item) => (
               <Marker key={item.id} position={item}>
-                <Popup>
+                <Popup key={item.id}>
                   <b> {item.name} </b>
                   <br />
                   {item.address}
+                  <br />
+                  {item.distance}
                 </Popup>
               </Marker>
             ))
           : null}
       </MapContainer>
+      <p className="read-the-docs">↓↓ALL トイレリスト↓↓</p>
+
+      {placeData.map((ele) => (
+          <div className="list-button" key={ele.id}>
+            <span style={{ color: "Royalblue" }}>{ele.name}</span> |{" "}
+            <span style={{ color: "Seagreen" }}>{ele.address}</span> |{" "}
+            <span style={{ color: "Darkred" }}>{ele.distance}</span>
+            {/* <button onClick={() => deleteMark(ele.id)}>削除</button> */}
+          </div>
+      ))}
     </>
   );
 };
--- server.js
+++ server.js
@@ -21,38 +21,30 @@ const allowCrossDomain = function (req, res, next) {
   };
   app.use(allowCrossDomain);
 
-const list = [
-    'test',
-    'nishizuka',
-    'yoshiki'
-]
-
-// const TOILETTES_TABLE = 'toilettes';
-
 app.use('/', express.static(__dirname + '/frontend/dist'))
 
-// app.get('/api/list', (req,res) => {
-//     res.json(list);
-// });
-
-app.post('/api/list',(req,res)=> {
+app.post('/api/list',async (req,res)=> {
   const body = req.body;
   console.log(body)
-  
-  // list.push(req.body)
-  // res.send(list)
-  res.end()
+  const lists = await db('toilettes')
+  .insert({
+    name:body.name,
+    address:body.address,
+    latitude:body.latitude,
+    longitude:body.longitude
+  })
+  res.end
 });
+
 app.get('/api/list', async (req,res) => {
     const lists = await db('toilettes')
     .select('id','name','latitude as lat','longitude as lng','address')
     .catch((err)=> {
         console.log(err);
     });
-    // const result = lists.map((ele)=> ele.name)
-    // console.log(lists)
     res.send(lists)
 });
+
 
 app.listen(PORT, ()=> {
     console.log(`server is working now. Port:${PORT}`)
